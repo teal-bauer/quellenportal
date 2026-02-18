988a880b7e6744d48e10dbb5594be46e42e0687a
Teal Bauer <teal@starsong.eu>
2026-02-18 23:14:36 +0100

    Fix: Remove invalid Meilisearch initializer

--- config/initializers/meilisearch.rb
+++
@@ -1,8 +0,0 @@
-MeiliSearch::Rails.configuration = {
-  meilisearch_url: ENV.fetch('MEILISEARCH_HOST', 'http://localhost:7700'),
-  meilisearch_api_key: ENV.fetch('MEILISEARCH_API_KEY', ''),
-  timeout: 10,
-  max_retries: 2,
-  pagination_backend: :kaminari,
-  per_environment: true
-}
